622e9c10ab769fc9b7707d8b8625dcac1f82e4dd
Luciphoenix <luciphoenix.soft@gmail.com>
2025-01-03 19:19:46 +0000

    Create grids inside container

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const container = document.querySelector('.container')
-const button = document.querySelector('button')
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-function askNumber(){
-    const gridNumber = prompt("Enter a number between 1 - 100")    
+function askNumber() {
+  const gridNumber = prompt("Enter a number between 1 - 100");
+  createGrid(gridNumber);
 }
 
-button.addEventListener('click', askNumber)
+button.addEventListener("click", askNumber);
 
+function createGrid(number) {
+  clearContainer();
+  for (let i = 0; i < number ** 2; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    div.style.width = `${800 / number}px`;
+    div.style.height = `${800 / number}px`;
+    container.appendChild(div);
+  }
+}
 
-
+function clearContainer() {
+  container.replaceChildren();
+}
