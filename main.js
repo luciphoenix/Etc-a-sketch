1d6d4550c8336c7301dd4af5cb23554a68361858
Luciphoenix <luciphoenix.soft@gmail.com>
2025-01-03 17:51:03 +0000

    Add a button to request number from user

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const container = document.querySelector('.container')
+const button = document.querySelector('button')
+
+function askNumber(){
+    const gridNumber = prompt("Enter a number between 1 - 100")    
+}
+
+button.addEventListener('click', askNumber)
+
+
 
