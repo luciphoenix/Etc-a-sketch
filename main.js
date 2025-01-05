dd46ec524646740735a193dcee6d96903cce577e
Luciphoenix <luciphoenix.soft@gmail.com>
2025-01-05 00:54:11 +0000

    Increase opacity upon each hover

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
 
-
-createGrid()
+createGrid();
 
 function createGrid(number = 16) {
   clearContainer();
@@ -21,29 +20,28 @@ function clearContainer() {
 
 function askNumber() {
   const gridNumber = prompt("Enter a number between 1 - 100");
-  gridNumber > 100 && askNumber() //Entered number must be less than 101
+  gridNumber > 100 && askNumber(); //Entered number must be less than 101
   createGrid(gridNumber);
 }
 
 button.addEventListener("click", askNumber);
-container.addEventListener('mouseover', addHover)
+container.addEventListener("mouseover", addHover);
 
-function addHover(e){
-  color = `rgb(${random()},${random()},${random()})`
-  e.target.style.backgroundColor= color
-  console.log(e.target);
- 
+function addHover(e) {
+  let alpha = +e.target.style.backgroundColor
+    .split(",")
+    .pop()
+    .slice(0, -1)
+    .trim();
+
+  color = `rgba(${random()},${random()},${random()}, ${alpha + 0.1})`;
+  e.target.style.backgroundColor = color;
 }
 
-container.addEventListener('mouseout', (e)=>{
- e.target.style.backgroundColor = ''
-})
+// container.addEventListener('mouseout', (e)=>{
+//  e.target.style.backgroundColor = ''
+// })
 
-function random(){
-  return Math.trunc(Math.random() * 255)
+function random() {
+  return Math.trunc(Math.random() * 255);
 }
-
-
-
-
-
